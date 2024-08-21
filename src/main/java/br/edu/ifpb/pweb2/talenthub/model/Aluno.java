518d238c266347662eb30116c0bb1a080157abfa
Luiz Manoel Dantas <dantasluizmanoel@gmail.com>
2024-08-20 21:36:16 -0300

    feat: gets, sets e construtores via lombok

--- src/main/java/br/edu/ifpb/pweb2/talenthub/model/Aluno.java
+++ src/main/java/br/edu/ifpb/pweb2/talenthub/model/Aluno.java
@@ -1,8 +1,16 @@
 package br.edu.ifpb.pweb2.talenthub.model;
 
-import jakarta.persistence.*;
 import java.util.Set;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Aluno {
 
@@ -25,53 +33,4 @@ public class Aluno {
     @OneToMany(mappedBy = "aluno")
     private Set<Estagio> estagios;
 
-    // Getters e Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public Set<String> getHabilidades() {
-        return habilidades;
-    }
-
-    public void setHabilidades(Set<String> habilidades) {
-        this.habilidades = habilidades;
-    }
-
-    public Set<Estagio> getEstagios() {
-        return estagios;
-    }
-
-    public void setEstagios(Set<Estagio> estagios) {
-        this.estagios = estagios;
-    }
 }
--- src/main/java/br/edu/ifpb/pweb2/talenthub/model/Empresa.java
+++ src/main/java/br/edu/ifpb/pweb2/talenthub/model/Empresa.java
@@ -3,6 +3,13 @@ package br.edu.ifpb.pweb2.talenthub.model;
 import jakarta.persistence.*;
 import java.util.Set;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Empresa {
 
@@ -26,69 +33,5 @@ public class Empresa {
     @OneToMany(mappedBy = "empresa")
     private Set<Oferta> ofertas;
 
-    // Getters e Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAtividadePrincipal() {
-        return atividadePrincipal;
-    }
-
-    public void setAtividadePrincipal(String atividadePrincipal) {
-        this.atividadePrincipal = atividadePrincipal;
-    }
-
-    public Set<Oferta> getOfertas() {
-        return ofertas;
-    }
-
-    public void setOfertas(Set<Oferta> ofertas) {
-        this.ofertas = ofertas;
-    }
+ 
 }
