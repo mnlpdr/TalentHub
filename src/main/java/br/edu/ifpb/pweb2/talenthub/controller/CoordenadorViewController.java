4ca086465a6dad8eae49f648d215665383df8b35
mnlpdr <mnlpdr.a@gmail.com>
2024-08-27 02:03:41 -0300

    lImplementado Login para Coordenador visualizar Ofertas

--- src/main/java/br/edu/ifpb/pweb2/talenthub/controller/CoordenadorViewController.java
+++ src/main/java/br/edu/ifpb/pweb2/talenthub/controller/CoordenadorViewController.java
@@ -1,4 +1,34 @@
 package br.edu.ifpb.pweb2.talenthub.controller;
 
+import br.edu.ifpb.pweb2.talenthub.model.Oferta;
+import br.edu.ifpb.pweb2.talenthub.service.OfertaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/coordenador")
 public class CoordenadorViewController {
+
+    @Autowired
+    private OfertaService ofertaService;
+
+    // Metodo para listar todas as ofertas de estágio
+    @GetMapping("/ofertas")
+    public String listarOfertas(Model model) {
+        List<Oferta> ofertas = ofertaService.listarTodos();
+        model.addAttribute("ofertas", ofertas);
+        return "oferta/listarOferta";
+    }
+
+    //Metodo para navegar para a página de login de coordenador
+    @GetMapping("/login")
+    public String mostrarPaginaDeLogin() {
+        return "coordenador/loginCoordenador";
+    }
+
 }
