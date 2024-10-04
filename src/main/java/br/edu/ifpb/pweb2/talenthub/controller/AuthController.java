859a98b0c1d80eea8bebfdb93e8888b5dabaa890
Luiz Manoel Dantas <dantasluizmanoel@gmail.com>
2024-10-04 15:02:39 -0300

    renomeando controller de login

--- src/main/java/br/edu/ifpb/pweb2/talenthub/controller/AuthController.java
+++ src/main/java/br/edu/ifpb/pweb2/talenthub/controller/AuthController.java
@@ -4,15 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
-
+public class AuthController {
     @GetMapping("/login")
     public String login() {
-        return "auth/login";  // Nome do template Thymeleaf
+        return "auth/login";
     }
 
     @GetMapping("/logout")
     public String logout() {
-        return "auth/login"; // Nome do template Thymeleaf
+        return "auth/login"; 
     }
 }
